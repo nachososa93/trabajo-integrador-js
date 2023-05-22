302b33b22ca5895edfd87a0179a82815aa23504f
nachososa93 <ignaciososa1020@gmail.com>
2023-05-22 20:41:27 -0300

    primer commit

---
+++ main.js
@@ -0,0 +1,171 @@
+const productos = [
+    {   nombre : "Barreno estandar",
+        id : 1,
+        categoria :"barrenos y extensores",
+        precio: "21.000",
+        imagen: ["./img-productos/barreno estandar.jpg", "./img-productos/barreno zoom 1.jpg", "./img-productos/barreno zoom 2.jpg"]
+
+    },
+
+    {   nombre : "Barreno reforzado",
+    id : 2,
+    categoria :"barrenos y extensores",
+    precio: "25.000",
+        imagen: "./img-productos/img-slide/barreno para golpear.jpg"
+    },
+
+    {   nombre : "Barreno reforzado y extensor",
+    id : 1,
+    categoria :"barrenos y extensores",
+    precio: "35.000",
+    imagen: "./img-slide/barreno estandar.jpg"
+    },
+
+    {   nombre : "Barreno estandar",
+    id : 1,
+    categoria :"barrenos y extensores",
+    precio: "21.000",
+    imagen: "./img-slide/barreno estandar.jpg"
+    },
+
+    {   nombre : "Barreno estandar",
+    id : 1,
+    categoria :"barrenos y extensores",
+    precio: "21.000",
+    imagen: "/img-slide/barreno estandar.jpg"
+    },
+
+    {   nombre : "Barreno estandar",
+    id : 1,
+    categoria :"barrenos y extensores",
+    precio: "21.000",
+    imagen: "/img-slide/barreno estandar.jpg"
+    },
+
+    {   nombre : "Barreno estandar",
+    id : 1,
+    categoria :"barrenos y extensores",
+    precio: "21.000",
+    imagen: "/img-slide/barreno estandar.jpg"
+    },
+    
+    {   nombre : "Barreno estandar",
+    id : 1,
+    categoria :"barrenos y extensores",
+    precio: "21.000",
+    imagen: "/img-slide/barreno estandar.jpg"
+    },
+
+    {   nombre : "Barreno estandar",
+    id : 1,
+    categoria :"barrenos y extensores",
+    precio: "21.000",
+    imagen: "/img-slide/barreno estandar.jpg"
+    },
+
+    {   nombre : "Barreno estandar",
+    id : 1,
+    categoria :"barrenos y extensores",
+    precio: "21.000",
+    imagen: "/img-slide/barreno estandar.jpg"
+    },
+
+    {   nombre : "Barreno estandar",
+    id : 1,
+    categoria :"barrenos y extensores",
+    precio: "21.000",
+    imagen: "/img-slide/barreno estandar.jpg"
+    },
+
+    {   nombre : "Barreno estandar",
+    id : 1,
+    categoria :"barrenos y extensores",
+    precio: "21.000",
+    imagen: "/img-slide/barreno estandar.jpg"
+    },
+
+    {   nombre : "Barreno estandar",
+    id : 1,
+    categoria :"barrenos y extensores",
+    precio: "21.000",
+    imagen: "/img-slide/barreno estandar.jpg"
+    }
+
+
+]
+
+
+
+
+
+console.log(./img-productos/barreno zoom 1.jpg);
+
+
+
+
+const renderProductos = ()=>{
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const init =()=> {
+renderProductos()
+}
+
+init ()
